d2d518b886c1d15cc6cd93ac921c3763a45a0dde
Nuhirath Rafthia <nr2793@columbia.edu>
2024-05-29 17:19:18 -0400

    Start building Sandwich class

--- src/main/java/com/ps/Sandwich.java
+++ src/main/java/com/ps/Sandwich.java
@@ -1,4 +1,26 @@
 package com.ps;
 
-public class Sandwich {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sandwich extends Product {
+    private String size;
+    private String bread;
+    private List<Topping> toppings = new ArrayList<>();
+    private boolean toasted;
+
+    public Sandwich(String size, String bread, boolean toasted) {
+        this.size = size;
+        this.bread = bread;
+        this.toasted = toasted;
+    }
+
+    public void addTopping(Topping topping) {
+        toppings.add(topping);
+    }
+
+    @Override
+    public double calcPrice() {
+        return 0;
+    }
 }
