b1ea434cc1b078202689f087a8122fb1946eb02c
Nuhirath Rafthia <nr2793@columbia.edu>
2024-05-29 19:23:12 -0400

    Update User Interface to include add sandwich and topping screen

--- src/main/java/com/ps/UserInterface.java
+++ src/main/java/com/ps/UserInterface.java
@@ -3,12 +3,13 @@ package com.ps;
 import java.util.Scanner;
 
 public class UserInterface {
-    private static final Scanner scanner = new Scanner(System.in);
+    private Order currentOrder = new Order();
+    private static Scanner scanner = new Scanner(System.in);
     public static void displayHomeScreen(){
         int selection;
         do{
             System.out.println("Welcome To The DELI-cious Sandwich Shop!");
-            System.out.println("Please select from the appropriate menu options: ");
+            System.out.println("Please select from the appropriate menu options:");
             System.out.println("------------------------------");
             System.out.println("1) New Order");
             System.out.println("0) Exit");
@@ -35,8 +36,8 @@ public class UserInterface {
     public static void displayOrderScreen() {
         int selection;
         do{
-            System.out.println("\nWelcome to The Order Screen!");
-            System.out.println("Please select from the appropriate menu options: ");
+            System.out.println("\nWelcome to the Order Screen!");
+            System.out.println("Please select from the appropriate menu options:");
             System.out.println("------------------------------");
             System.out.println("1) Add Sandwich");
             System.out.println("2) Add Drink");
@@ -73,8 +74,101 @@ public class UserInterface {
     }
 
     public static void displayAddSandwichScreen() {
+        System.out.println("\nWelcome to the Sandwich Screen!");
+        System.out.println("Please select your bread type:");
+        System.out.println("------------------------------");
+        System.out.println("1) White");
+        System.out.println("2) Wheat");
+        System.out.println("3) Rye");
+        System.out.println("4) Wrap");
+        System.out.println("------------------------------");
+
+        System.out.print("Selection: ");
+        String bread = getBreadType(scanner.nextInt());
+        scanner.nextLine();
+
+        System.out.println("Select your sandwich size:");
+        System.out.println("1) 4\"");
+        System.out.println("2) 8\"");
+        System.out.println("3) 12\"");
+        String size = getSandwichSize(scanner.nextInt());
+        scanner.nextLine();
+
+        System.out.println("Would you like the sandwich toasted?");
+        System.out.println("Yes");
+        System.out.println("No");
+        boolean toasted = scanner.nextLine().equalsIgnoreCase("yes");
+
+        Sandwich sandwich = new Sandwich(size, bread, toasted);
+        displayAddToppingsScreen(sandwich);
     }
 
+    public static String getBreadType(int selection){
+        // IntelliJ enhanced switch suggestion
+        return switch (selection) {
+            case 1 -> "White";
+            case 2 -> "Wheat";
+            case 3 -> "Rye";
+            case 4 -> "Wrap";
+            default -> "Invalid";
+        };
+    }
+
+    public static String getSandwichSize(int selection){
+        // IntelliJ enhanced switch suggestion
+        return switch (selection) {
+            case 1 -> "4\"";
+            case 2 -> "8\"";
+            case 3 -> "12\"";
+            default -> "Invalid";
+        };
+    }
+
+    private static void displayAddToppingsScreen(Sandwich sandwich) {
+        int selection;
+        while (true) {
+            System.out.println("Welcome to the Toppings Screen!");
+            System.out.println("Please select your topping type:");
+            System.out.println("------------------------------");
+            System.out.println("1) Meat (steak, ham, salami, roast beef, chicken, bacon)");
+            System.out.println("2) Cheese (american, provolone, cheddar, swiss)");
+            System.out.println("3) Regular Toppings (lettuce, peppers, onions, tomatoes, jalapenos, cucumbers, pickles, guacamole, mushrooms)");
+            System.out.println("4) Sauces (mayo, mustard, ketchup, ranch, thousand islands, vinaigrette)");
+            System.out.println("0) Done adding toppings");
+            System.out.println("------------------------------");
+            selection = scanner.nextInt();
+            scanner.nextLine();
+
+            if (selection == 0) {
+                break;
+            } else {
+                System.out.println("Enter the name of the topping:");
+                String toppingName = scanner.nextLine();
+                String toppingType = getToppingType(selection);
+                boolean isPremium = false;
+                boolean isExtra = false;
+                if (toppingType.equals("Meat") || toppingType.equals("Cheese")){
+                    isPremium = true;
+                    System.out.println("Would you like to add extra?");
+                    System.out.println("Yes");
+                    System.out.println("No");
+                    isExtra = scanner.nextLine().equalsIgnoreCase("yes");
+                }
+                sandwich.addTopping(new Topping(toppingName, toppingType, isPremium, isExtra));
+            }
+        }
+    }
+
+    private static String getToppingType(int selection){
+        return switch (selection) {
+            case 1 -> "Meat";
+            case 2 -> "Cheese";
+            case 3 -> "Regular Topping";
+            case 4 -> "Sauce";
+            case 5 -> "Side";
+            default -> "Invalid";
+        };
+    }
     private static void displayAddDrinkScreen() {
     }
 
