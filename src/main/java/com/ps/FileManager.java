c490e2c6864128db201aca36d65ee22985ff3f39
Nuhirath Rafthia <nr2793@columbia.edu>
2024-05-30 11:20:04 -0400

    Work on File Manager class to fix issues generating receipt files

--- src/main/java/com/ps/FileManager.java
+++ src/main/java/com/ps/FileManager.java
@@ -8,8 +8,13 @@ import java.io.IOException;
 public class FileManager {
     public static void saveOrder(Order order) {
         try {
+            File directory = new File("receipts");
+            if (!directory.exists()) {
+                directory.mkdirs();
+            }
             BufferedWriter buffWriter = new BufferedWriter(new FileWriter("receipts/" + order.getOrderID() + ".txt"));
             buffWriter.write(order.toString());
+            buffWriter.close();
         } catch (IOException e) {
             System.out.println("An error occurred while saving the order.");
             e.printStackTrace();
